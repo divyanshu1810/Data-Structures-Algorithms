22cd09bd4a506691e9897118f6705032c5e0ba77
Divyanshu Yadav <divyanshu.1810@gmaiil.com>
2022-12-16 12:54:12 +0530

    :sparkles: Course Begin
    Let's Start the Course

---
+++ arrays/Main.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming.arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] intArray = new int[7];
+
+	    intArray[0] = 20;
+	    intArray[1] = 35;
+	    intArray[2] = -15;
+	    intArray[3] = 7;
+	    intArray[4] =55;
+	    intArray[5] = 1;
+	    intArray[6] = -22;
+
+	    int index = -1;
+	    for (int i = 0; i < intArray.length; i++) {
+	        if (intArray[i] == 7) {
+	            index = i;
+	            break;
+            }
+        }
+
+        System.out.println("index = " + index);
+
+
+    }
+}
